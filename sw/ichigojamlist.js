3b78997b5103eecc7cbe62381be2d4a5f052f971
H.Hara <harahidekazu@gmail.com>
2020-07-10 23:56:33 +0900

    added : 1st trial Swahili language version. It's forked from en folder.

---
+++ sw/ichigojamlist.js
@@ -0,0 +1,424 @@
+// http://fukuno.jig.jp/1423
+// CC BY fukuno.jig.jp
+
+var f2s = function(f) {
+	return f.toString().match(/\n([\s\S]*)\n/)[1];
+};
+var ichigojamfont_data = f2s(function() { /*
+0000000000000000
+ffffffffffffffff
+ffaaff55ffaaff55
+55aa55aa55aa55aa
+005500aa005500aa
+995a3c5a5a242466
+fbfbfb00dfdfdf00
+24182424183c6624
+0a042a40fe402000
+000000000000ee00
+00042464fc602000
+eebaee447c447c44
+1042008001004208
+007e7e7e7e7e7e00
+007e424242427e00
+007e5e5e5e427e00
+007e7a7a6a427e00
+003c242424243c00
+c0c0c0c0c0c0c0c0
+ffff000000000000
+000000000000ffff
+003c3c4242423c00
+003c665e5e663c00
+0303030303030303
+0000ff0000ff0000
+03070e1c3870e0c0
+c0e070381c0e0703
+606c34f018284e40
+102040fe40201000
+100804fe04081000
+1038549210101000
+1010109254381000
+0000000000000000
+1010101010001000
+2828000000000000
+28287c287c282800
+103c503814781000
+60640810204c0c00
+2050502054483400
+0810200000000000
+0810202020100800
+2010080808102000
+1054381038541000
+0010107c10100000
+0000000010102000
+0000007c00000000
+0000000000303000
+0000040810204000
+38444c5464443800
+1030501010107c00
+3844040418607c00
+3844041804443800
+18284848487c0800
+7c40780404443800
+3840784444443800
+7c44040808101000
+3844443844443800
+384444443c043800
+0000100000100000
+0000100010102000
+0810204020100800
+00007c007c000000
+2010080408102000
+3844440810001000
+3844043454543800
+384444447c444400
+7824243824247800
+3844404040443800
+7824242424247800
+7c40407c40407c00
+7c40407c40404000
+384440404c443c00
+4444447c44444400
+3810101010103800
+1c08080808483000
+4448506050484400
+4040404040407c00
+446c6c5454544400
+446464544c4c4400
+3844444444443800
+7844444478404000
+3844444454483400
+7844444478484400
+3844403804443800
+7c10101010101000
+4444444444443800
+4444282828101000
+4444545454282800
+4444281028444400
+4444281010101000
+7c04081020407c00
+3820202020203800
+0000402010080400
+3808080808083800
+1028440000000000
+0000000000007c00
+2010080000000000
+000038043c443a00
+4040586444447800
+0000384440443800
+0404344c44443c00
+000038447c403800
+1820207c20202000
+00003a44443c0438
+4040586444444400
+1000301010101000
+0800180808080830
+2020242830282400
+3010101010101800
+0000785454545400
+0000784444444400
+0000384444443800
+0000384444784040
+00003844443c0404
+0000586440404000
+00003c4038047800
+20207c2020201800
+0000484848483400
+0000444428281000
+0000445454282800
+0000442810284400
+0000444428281060
+00007c0810207c00
+0c10102010100c00
+1010101010101000
+6010100810106000
+0000205408000000
+a040a804fe040800
+0000000000000000
+f0f0f0f000000000
+0f0f0f0f00000000
+ffffffff00000000
+00000000f0f0f0f0
+f0f0f0f0f0f0f0f0
+0f0f0f0ff0f0f0f0
+fffffffff0f0f0f0
+000000000f0f0f0f
+f0f0f0f00f0f0f0f
+0f0f0f0f0f0f0f0f
+ffffffff0f0f0f0f
+00000000ffffffff
+f0f0f0f0ffffffff
+0f0f0f0fffffffff
+ffffffffffffffff
+0000001818000000
+000000ffff000000
+1818181818181818
+181818ffff181818
+181818f8f8181818
+1818181f1f181818
+181818ffff000000
+000000ffff181818
+0000000f1f181818
+000000f0f8181818
+1818181f0f000000
+181818f8f0000000
+fffefcf8f0e0c080
+ff7f3f1f0f070301
+80c0e0f0f8fcfeff
+0103070f1f3f7fff
+44287c107c101000
+0000000070507000
+0e08080000000000
+0000000010107000
+0000000040201000
+0000001818000000
+007e027e02041800
+0000007c14102000
+0000000c70101000
+0000107c44041800
+0000007c10107c00
+0000087c18284800
+0000207c24202000
+0000003808087c00
+00003c043c043c00
+0000005454040800
+000000007e000000
+00fe021410106000
+0006186808080800
+107e424202041800
+007c10101010fe00
+04047e0c14244400
+10107e1212224600
+10107e107e101000
+003e224202043800
+20203e4404043800
+00007e0202027e00
+0044fe4444043800
+0070027202047800
+007e020408146200
+0040fe4448403e00
+0042422404081000
+003e22520a043800
+043808fe08083000
+0052525202041800
+007c00fe08087000
+404040704c404000
+0008fe0808087000
+00007c000000fe00
+007e023408146200
+107e020418761000
+0002020202047800
+0028284444828200
+00404e7040403e00
+007e020202043800
+0000205088040200
+0010fe1054549200
+00fe024428100800
+00700e700e700e00
+001010202442fe00
+0002221408146200
+007c20fe20201e00
+2020fe2224202000
+00003c0404047e00
+007c047c04047c00
+007e007e02043800
+0044444404083000
+0050505052949800
+0020202224283000
+007e424242427e00
+007e424202043800
+0040220202047800
+1048200000000000
+7050700000000000
+183878ffff783818
+181c1effff1e1c18
+183c7effff181818
+181818ffff7e3c18
+10387cfefe387c00
+006cfefe7c381000
+3838d6fed6103800
+10387cfe7c381000
+3c66c38181c3663c
+3c7effffffff7e3c
+246a2a2a2a2a2400
+18244281bdbdbd7e
+245a4281a581423c
+3c4281a5817e2466
+0c0a0a0878f87000
+3c4299a5ada1924c
+181824247eff3c7e
+00182442ff540000
+1010080810100808
+7c101eb9ff9f107e
+085a6cfe3c7e4a11
+1c363a3a3a3e1c00
+003c427e5a427e00
+0006061e1e7e7e00
+007c446464447c00
+18183c5a5a242466
+00187e99183c2466
+00181a7e501c1466
+1818101010101018
+0018587e0a182e62
+1818080808080818
+043e2f566ad6acf0
+*/});
+var ichigojamfont;
+{
+	var font = ichigojamfont_data.split("\n");
+	
+	var bfont = [];
+	for (var j = 0; j < font.length; j++) {
+		var code = font[j];
+		var bf = [];
+		for (var i = 0; i < 8; i++) {
+			var n = parseInt(code.substring(i * 2, i * 2 + 2), 16);
+			bf.push(n);
+		}
+		bfont.push(bf);
+	}
+	ichigojamfont = bfont;
+}
+
+var SVG_class = function() {
+	this.bgcolor = "black";
+	this.color = "white";
+	this.createRect = function(x, y, w, h) {
+		var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+		rect.setAttribute("x", x);
+		rect.setAttribute("y", y);
+		rect.setAttribute("width", w);
+		rect.setAttribute("height", h);
+		rect.setAttribute("fill", this.color);
+		return rect;
+	};
+	this.create = function(w, h) {
+		var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+		svg.setAttribute("width", w);
+		svg.setAttribute("height", h);
+//		svg.setAttribute("fill", this.bgcolor);
+		svg.style.backgroundColor = this.bgcolor;
+		return svg;
+	};
+	this.appendFont = function(svg, c, x, y, w, h, w2, h2) {
+		var code = ichigojamfont[c];
+		var r = w / 8;
+		if (!w2)
+			w2 = w;
+		if (!h2)
+			h2 = h;
+		for (var i = 0; i < 8; i++) {
+			var n = code[i];
+			for (var j = 0; j < 8; j++) {
+				if (n & (1 << (7 - j))) {
+					svg.appendChild(this.createRect(x + j * w, y + i * h, w2, h2));
+				}
+			}
+		}
+	};
+	this.makeTest = function() {
+		var s = 4;
+		var svg = this.create(s * 8 * 16, s * 8 * 16);
+		for (var i = 0; i < 16; i++) {
+			for (var j = 0; j < 16; j++) {
+				this.appendFont(svg, i + j * 16, i * s * 8, j * s * 8, 4, 4);
+			}
+		}
+//		this.appendFont(svg, 224, 10, 10, 4, 4);
+	//	svg.appendChild(createSVGRect(10, 10, 30, 20));
+	//	svg.appendChild(createSVGRect(20, 30, 10, 10));
+		return svg;
+	};
+	this.makeLIST = function(s, cw) {
+		var skip = 1;
+		var margin = 2;
+		var w = 2;
+		var w2 = w;
+		if (cw) {
+			w = cw / (8 * 32 + margin * 2);
+			w2 = w;
+			if (w != Math.floor(w))
+				w2 = w * 1.15;
+		}
+		s = toHalf(s);
+		
+		var svg = this.create(w * (8 * 32 + margin * 2), 0);
+		var x = 0;
+		var y = 0;
+		var flg = false;
+		for (var i = 0; i < s.length; i++) {
+			var c = s.charCodeAt(i);
+			if (c >= 0xff61 && c <= 0xff9f) { // 半角カナ コード変換
+				c -= 0xff00 - 0x40;
+			}
+			if (c != 10 || !flg)
+				this.appendFont(svg, c, (x * 8 + margin) * w, (y * (8 + skip) + margin) * w, w, w, w2, w2);
+			if (c == 10) {
+				x = 0;
+				y++;
+			} else {
+				x++;
+				if (x == 32) {
+					x = 0;
+					y++;
+				}
+				flg = c == 32;
+			}
+		}
+		if (c > 1 && c != 0x85 && !flg) {
+			this.appendFont(svg, 10, (x * 8 + margin) * w, (y * (8 + skip) + margin) * w, w, w, w2, w2);
+		}
+		y++;
+		svg.setAttribute("height", w * (y * (8 + skip) - skip + margin * 2));
+		
+		if (this.color == "black") {
+			var ww = w / 2;
+			var sw = (32 * 8 + margin * 2) * w;
+			var sh = (y * (8 + skip) - skip + margin * 2) * w;
+			svg.appendChild(this.createRect(0, 0, sw, ww));
+			svg.appendChild(this.createRect(0, sh - ww, sw, ww));
+			svg.appendChild(this.createRect(0, 0, ww, sh));
+			svg.appendChild(this.createRect(sw - ww, 0, ww, sh));
+		}
+		return svg;
+	};
+};
+
+var SVG = new SVG_class();
+var makeIchigoJamLIST = function(id, s) {
+	var c = document.getElementById(id);
+	c.appendChild(SVG.makeLIST(s, c.clientWidth));
+};
+var makeIchigoJamLISTblack = function(id, s) {
+	SVG.color = "black";
+	SVG.bgcolor = "white";
+	var c = document.getElementById(id);
+	c.appendChild(SVG.makeLIST(s, c.clientWidth));
+};
+var makeIchigoJamLISTblackSize2 = function(id, s) {
+	SVG.color = "black";
+	SVG.bgcolor = "white";
+	var c = document.getElementById(id);
+	c.appendChild(SVG.makeLIST(s, (256 + 4) * 2));
+};
+var make15 = makeIchigoJamLISTblackSize2;
+
+// util
+var toHalf = function(s) {
+	var zen = [ "ガ", "ギ", "グ", "ゲ", "ゴ", "ザ", "ジ", "ズ", "ゼ", "ゾ", "ダ", "ヂ", "ヅ", "デ", "ド", "バ", "パ", "ビ", "ピ", "ブ", "プ", "ベ", "ペ", "ボ", "ポ", "ヴ", "ァ", "ア", "ィ", "イ", "ゥ", "ウ", "ェ", "エ", "ォ", "オ", "カ", "キ", "ク", "ケ", "コ", "サ", "シ", "ス", "セ", "ソ", "タ", "チ", "ッ", "ツ", "テ", "ト", "ナ", "ニ", "ヌ", "ネ", "ノ", "ハ", "ヒ", "フ", "ヘ", "ホ", "マ", "ミ", "ム", "メ", "モ", "ャ", "ヤ", "ュ", "ユ", "ョ", "ヨ", "ラ", "リ", "ル", "レ", "ロ", "ワ", "ヲ", "ン", "。", "「", "」", "、", "・", "ー", "゛", "゜" ];
+	var han = [ "ｶﾞ", "ｷﾞ", "ｸﾞ", "ｹﾞ", "ｺﾞ", "ｻﾞ", "ｼﾞ", "ｽﾞ", "ｾﾞ", "ｿﾞ", "ﾀﾞ", "ﾁﾞ", "ﾂﾞ", "ﾃﾞ", "ﾄﾞ", "ﾊﾞ", "ﾊﾟ", "ﾋﾞ", "ﾋﾟ", "ﾌﾞ", "ﾌﾟ", "ﾍﾞ", "ﾍﾟ", "ﾎﾞ", "ﾎﾟ", "ｳﾞ", "ｧ", "ｱ", "ｨ", "ｲ", "ｩ", "ｳ", "ｪ", "ｴ", "ｫ", "ｵ", "ｶ", "ｷ", "ｸ", "ｹ", "ｺ", "ｻ", "ｼ", "ｽ", "ｾ", "ｿ", "ﾀ", "ﾁ", "ｯ", "ﾂ", "ﾃ", "ﾄ", "ﾅ", "ﾆ", "ﾇ", "ﾈ", "ﾉ", "ﾊ", "ﾋ", "ﾌ", "ﾍ", "ﾎ", "ﾏ", "ﾐ", "ﾑ", "ﾒ", "ﾓ", "ｬ", "ﾔ", "ｭ", "ﾕ", "ｮ", "ﾖ", "ﾗ", "ﾘ", "ﾙ", "ﾚ", "ﾛ", "ﾜ", "ｦ", "ﾝ", "｡", "｢", "｣", "､", "･", "ｰ", "ﾞ", "ﾟ" ];
+	var res = "";
+	for (var i = 0; i < s.length; i++) {
+		var c = s.substring(i, i + 1);
+		var flg = false;
+		for (var j = 0; j < zen.length; j++) {
+			if (c == zen[j]) {
+				res += han[j];
+				flg = true;
+				break;
+			}
+		}
+		if (!flg)
+			res += c;
+	}
+	return res;
+};
